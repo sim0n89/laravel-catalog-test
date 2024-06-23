46a36e6fb1b83aa565ae4b830a7c59b76ccd290d
sim0n89 <52407038+sim0n89@users.noreply.github.com>
2024-06-23 19:21:31 +0300

    переработана фильтрация

--- app/Filters/ProductFilter.php
+++ app/Filters/ProductFilter.php
@@ -2,18 +2,20 @@
 
 namespace App\Filters;
 
-use App\Models\Product;
-
 class ProductFilter extends QueryFilter
 {
     public function property($props = [])
     {
-        $products = $this->builder->with('properties')->whereHas('properties', function($query) use($props){
-            foreach ($props as $propId=>$propValues) {
-                $query->where('product_property.property_id', $propId)->whereIn('value', $propValues);
+
+            $query = $this->builder->with('properties');
+
+            foreach ($props as $propId => $propValues) {
+                $query->whereHas('properties', function($query) use($propId, $propValues){
+                    $query->where('product_property.property_id', $propId)->whereIn('value', $propValues);
+                });
             }
-        })
-        ->get();
-        return $products;
+
+            $products = $query->get();
+            return $products;
     }
 }
